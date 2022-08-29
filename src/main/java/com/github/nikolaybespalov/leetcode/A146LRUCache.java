17b1bb5b25c4d1e4622db6ca04529602329c97b9
Nikolay Bespalov <nikolaybespalov@gmail.com>
2022-08-29 18:51:31 +0300

    146. LRU Cache

---
+++ src/main/java/com/github/nikolaybespalov/leetcode/A146LRUCache.java
@@ -0,0 +1,108 @@
+package com.github.nikolaybespalov.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * @see <a href="https://leetcode.com/problems/lru-cache/">146. LRU Cache</a>
+ */
+public class A146LRUCache {
+    public static class LRUCache {
+        static class MyLinkedNode<T> {
+            MyLinkedNode<T> next;
+            MyLinkedNode<T> prev;
+            T value;
+
+            public MyLinkedNode(T value) {
+                this.value = value;
+            }
+        }
+
+        static class KeyValue {
+            Integer key;
+            Integer value;
+
+            public KeyValue(Integer key, Integer value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private final HashMap<Integer, MyLinkedNode<KeyValue>> keyToNode = new HashMap<>();
+        private MyLinkedNode<KeyValue> head;
+        private MyLinkedNode<KeyValue> tail;
+        private final int capacity;
+
+        public LRUCache(int capacity) {
+            if (capacity < 0) {
+                throw new RuntimeException("Invalid capacity");
+            }
+
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            MyLinkedNode<KeyValue> node = keyToNode.get(key);
+
+            if (node == null) {
+                return -1;
+            }
+
+            moveToHead(node);
+
+            return head.value.value;
+        }
+
+        public void put(int key, int value) {
+            if (keyToNode.containsKey(key)) {
+                MyLinkedNode<KeyValue> node = keyToNode.get(key);
+
+                node.value.value = value;
+
+                moveToHead(node);
+
+                return;
+            }
+
+            if (keyToNode.size() == capacity) {
+                keyToNode.remove(tail.value.key);
+
+                if (tail.prev != null) {
+                    tail = tail.prev;
+                    tail.next = null;
+                }
+            }
+
+            if (keyToNode.size() == 0) {
+                head = new MyLinkedNode<>(new KeyValue(key, value));
+                tail = head;
+            } else {
+                MyLinkedNode<KeyValue> node = new MyLinkedNode<>(new KeyValue(key, value));
+
+                node.next = head;
+                head.prev = node;
+                head = node;
+            }
+
+            keyToNode.put(key, head);
+        }
+
+        private void moveToHead(MyLinkedNode<KeyValue> node) {
+            if (node.prev == null) {
+                return;
+            } else if (node.next == null) {
+                tail = node.prev;
+                tail.next = null;
+            } else {
+                MyLinkedNode<KeyValue> next = node.next;
+                MyLinkedNode<KeyValue> prev = node.prev;
+                node.prev.next = next;
+                node.next.prev = prev;
+            }
+
+            node.prev = null;
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+    }
+}
---
+++ src/test/java/com/github/nikolaybespalov/leetcode/A146LRUCacheTest.java
@@ -0,0 +1,109 @@
+package com.github.nikolaybespalov.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class A146LRUCacheTest {
+
+    @Test
+    public void test1() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(123);
+
+        assertEquals(-1, instance.get(0));
+        instance.put(0, 0);
+        assertEquals(0, instance.get(0));
+    }
+
+    @Test
+    public void test2() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(3);
+
+        instance.put(0, 0);
+        instance.put(1, 1);
+        instance.put(2, 2);
+        assertEquals(0, instance.get(0));
+        assertEquals(1, instance.get(1));
+        assertEquals(2, instance.get(2));
+    }
+
+    @Test
+    public void test3() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(3);
+
+        instance.put(0, 0);
+        instance.put(1, 1);
+        instance.put(2, 2);
+        instance.put(3, 3);
+        assertEquals(-1, instance.get(0));
+        assertEquals(1, instance.get(1));
+        assertEquals(2, instance.get(2));
+        assertEquals(3, instance.get(3));
+    }
+
+    @Test
+    public void test4() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(2);
+
+        instance.put(1, 1);
+        instance.put(2, 2);
+        assertEquals(1, instance.get(1));
+        instance.put(3, 3);
+        assertEquals(-1, instance.get(2));
+        instance.put(4, 4);
+        assertEquals(-1, instance.get(1));
+        assertEquals(3, instance.get(3));
+        assertEquals(4, instance.get(4));
+    }
+
+    @Test
+    public void test5() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(1);
+
+        instance.put(2, 1);
+        assertEquals(1, instance.get(2));
+    }
+
+    @Test
+    public void test6() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(2);
+
+        assertEquals(-1, instance.get(2));
+        instance.put(2, 6);
+        assertEquals(-1, instance.get(1));
+        instance.put(1, 5);
+        instance.put(1, 2);
+        assertEquals(2, instance.get(1));
+        assertEquals(6, instance.get(2));
+    }
+
+    @Test
+    public void test7() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(2);
+
+        instance.put(2, 1);
+        instance.put(3, 2);
+        assertEquals(2, instance.get(3));
+        assertEquals(1, instance.get(2));
+        instance.put(4, 3);
+        assertEquals(1, instance.get(2));
+        assertEquals(-1, instance.get(3));
+        assertEquals(3, instance.get(4));
+    }
+
+    @Test
+    public void test8() {
+        A146LRUCache.LRUCache instance = new A146LRUCache.LRUCache(1);
+
+        assertEquals(-1, instance.get(6));
+        assertEquals(-1, instance.get(8));
+        instance.put(12, 1);
+        assertEquals(-1, instance.get(2));
+        instance.put(15, 11);
+        instance.put(5, 2);
+        instance.put(1, 15);
+        instance.put(4, 2);
+        assertEquals(-1, instance.get(5));
+        instance.put(15, 15);
+    }
+}
