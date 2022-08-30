d6f98e706b9ca44a0b5e160088d5704fcc17f876
Nikolay Bespalov <nikolaybespalov@gmail.com>
2022-08-30 19:04:48 +0300

    460. LFU Cache

--- src/main/java/com/github/nikolaybespalov/leetcode/A460LFUCache.java
+++ src/main/java/com/github/nikolaybespalov/leetcode/A460LFUCache.java
@@ -31,7 +31,6 @@ public class A460LFUCache {
     private final int capacity;
     private final HashMap<Integer, MyLinkedNode<KeyValue>> keyToNode = new HashMap<>();
     private final HashMap<Integer, MyLinkedNode<KeyValue>> cntToHead = new HashMap<>();
-//    private MyLinkedNode<KeyValue> head;
     private MyLinkedNode<KeyValue> tail;
 
     public A460LFUCache(int capacity) {
@@ -40,44 +39,53 @@ public class A460LFUCache {
         }
 
         this.capacity = capacity;
-        this.cntToHead.put(1, null);
     }
 
     public int get(int key) {
+        if (capacity == 0) {
+            return -1;
+        }
+
         MyLinkedNode<KeyValue> node = keyToNode.get(key);
 
         if (node == null) {
             return -1;
         }
 
+        MyLinkedNode<KeyValue> prev = removeNode(node);
+
         node.data.cnt++;
 
-        moveToHead(node);
-
-        cntToHead.put(node.data.cnt, node);
+        putNode(node, prev);
 
         return node.data.value;
     }
 
     public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+
         if (keyToNode.containsKey(key)) {
             MyLinkedNode<KeyValue> node = keyToNode.get(key);
 
             node.data.value = value;
+
+            MyLinkedNode<KeyValue> prev = removeNode(node);
+
             node.data.cnt++;
 
-            moveToHead(node);
+            putNode(node, prev);
 
             return;
         }
 
+        MyLinkedNode<KeyValue> prev = null;
+
         if (keyToNode.size() == capacity) {
             keyToNode.remove(tail.data.key);
 
-            if (tail.prev != null) {
-                tail = tail.prev;
-                tail.next = null;
-            }
+            prev = removeNode(tail);
         }
 
         MyLinkedNode<KeyValue> head;
@@ -89,18 +97,91 @@ public class A460LFUCache {
             MyLinkedNode<KeyValue> node = new MyLinkedNode<>(new KeyValue(key, value));
             head = cntToHead.get(1);
 
-            node.next = head;
-            head.prev = node;
+            if (head != null) {
+                if (head.prev != null) {
+                    head.prev.next = node;
+                    node.prev = head.prev;
+                }
+                node.next = head;
+                head.prev = node;
+            } else {
+                if (prev != null) {
+                    node.prev = prev;
+                    prev.next = node;
+                } else {
+                    int asd = 0;
+                    int asdf = asd;
+                }
+            }
+
             head = node;
+
+            if (node.next == null) {
+                tail = node;
+            }
         }
 
-        cntToHead.put(head.data.cnt, head);
+        cntToHead.put(1, head);
         keyToNode.put(key, head);
     }
 
-    private void moveToHead(MyLinkedNode<KeyValue> node, MyLinkedNode<KeyValue> head) {
+    private void putNode(MyLinkedNode<KeyValue> node, MyLinkedNode<KeyValue> prev2) {
+//        if (node.prev == null) {
+//            return;
+//        }
+
+        MyLinkedNode<KeyValue> head = cntToHead.get(node.data.cnt);
+
+        if (head != null) {
+            if (head.prev != null) {
+                head.prev.next = node;
+                node.prev = head.prev;
+            }
+
+            head.prev = node;
+            node.next = head;
+        } else {
+            // Тут нужно иметь такой, что минимальный близкий справа
+            MyLinkedNode<KeyValue> next = cntToHead.get(node.data.cnt - 1);
+
+            if (next != null) {
+                node.next = next;
+                next.prev = node;
+            } else {
+                node.next = null;
+            }
+
+            // Тут нужно иметь такой что максимально близкий слева
+            MyLinkedNode<KeyValue> prev = cntToHead.get(node.data.cnt + 1);
+
+            if (prev != null) {
+                node.prev = prev;
+                prev.next = node;
+            } else {
+                node.prev = null;
+            }
+        }
+
+        head = node;
+
+        cntToHead.put(head.data.cnt, head);
+    }
+
+    private MyLinkedNode<KeyValue> removeNode(MyLinkedNode<KeyValue> node) {
+        if (node.prev == null && node.next == null ||
+                node.next == null && node.data.cnt < node.prev.data.cnt ||
+                node.prev == null && node.data.cnt > node.next.data.cnt ||
+                node.prev != null && node.next != null && node.data.cnt < node.prev.data.cnt && node.data.cnt > node.next.data.cnt) {
+            cntToHead.remove(node.data.cnt);
+        }
+
+        if (node.prev == null && node.next != null ||
+                node.prev != null && node.data.cnt < node.prev.data.cnt && node.next != null) {
+            cntToHead.put(node.next.data.cnt, node.next);
+        }
+
         if (node.prev == null) {
-            //return;
+            return null;
         } else if (node.next == null) {
             tail = node.prev;
             tail.next = null;
@@ -111,15 +192,11 @@ public class A460LFUCache {
             node.next.prev = prev;
         }
 
-        node.prev = null;
-        node.next = head;
-        head.prev = node;
-        head = node;
+        MyLinkedNode<KeyValue> prev = node.prev;
 
-        cntToHead.put(head.data.cnt, head);
-    }
+        //node.prev = null;
+        //node.next = null;
 
-    private void moveToHead(MyLinkedNode<KeyValue> node) {
-        moveToHead(node, cntToHead.get(node.data.cnt));
+        return prev;
     }
 }
--- src/test/java/com/github/nikolaybespalov/leetcode/A460LFUCacheTest.java
+++ src/test/java/com/github/nikolaybespalov/leetcode/A460LFUCacheTest.java
@@ -101,4 +101,154 @@ public class A460LFUCacheTest {
         assertEquals(6, instance.get(2));
     }
 
+    @Test
+    public void test9() {
+        A460LFUCache instance = new A460LFUCache(10);
+        instance.put(10, 13);
+        instance.put(3, 17);
+        instance.put(6, 11);
+        instance.put(10, 5);
+        instance.put(9, 10);
+        assertEquals(-1, instance.get(13));
+        instance.put(2, 19);
+        assertEquals(19, instance.get(2));
+        assertEquals(17, instance.get(3));
+        instance.put(5, 25);
+        assertEquals(-1, instance.get(8));
+        instance.put(9, 22);
+        instance.put(5, 5);
+        instance.put(1, 30);
+        assertEquals(-1, instance.get(11));
+        instance.put(9, 12);
+        assertEquals(-1, instance.get(7));
+        assertEquals(5, instance.get(5));
+        assertEquals(-1, instance.get(8));
+        assertEquals(12, instance.get(9));
+        instance.put(4, 30);
+        instance.put(9, 3);
+        assertEquals(3, instance.get(9));
+        assertEquals(5, instance.get(10));
+        assertEquals(5, instance.get(10));
+        instance.put(6, 14);
+        instance.put(3, 1);
+        assertEquals(1, instance.get(3));
+        instance.put(10, 11);
+        assertEquals(-1, instance.get(8));
+        instance.put(2, 14);
+        assertEquals(30, instance.get(1));
+        assertEquals(5, instance.get(5));
+        assertEquals(30, instance.get(4));
+        instance.put(11, 4);
+        instance.put(12, 24);
+        instance.put(5, 18);
+        assertEquals(-1, instance.get(13));
+        instance.put(7, 23);
+        assertEquals(-1, instance.get(8));
+        assertEquals(24, instance.get(12));
+        instance.put(3, 27);
+        instance.put(2, 12);
+        assertEquals(18, instance.get(5));
+        instance.put(2, 9);
+        instance.put(13, 4);
+        instance.put(8, 18);
+        instance.put(1, 7);
+        assertEquals(14, instance.get(6));
+        instance.put(9, 29);
+        instance.put(8, 21);
+        assertEquals(18, instance.get(5));
+        instance.put(6, 30);
+        instance.put(1, 12);
+        assertEquals(11, instance.get(10));
+        instance.put(4, 15);
+        instance.put(7, 22);
+        instance.put(11, 26);
+        instance.put(8, 17);
+        instance.put(9, 29);
+        assertEquals(18, instance.get(5));
+        instance.put(3, 4);
+        instance.put(11, 30);
+        assertEquals(-1, instance.get(12));
+        instance.put(4, 29);
+        assertEquals(4, instance.get(3));
+        assertEquals(29, instance.get(9));
+        assertEquals(30, instance.get(6));
+        instance.put(3, 4);
+        assertEquals(12, instance.get(1));
+        assertEquals(11, instance.get(10));
+        instance.put(3, 29);
+        instance.put(10, 28);
+        instance.put(1, 20);
+        instance.put(11, 13);
+        assertEquals(29, instance.get(3));
+        instance.put(3, 12);
+        instance.put(3, 8);
+        instance.put(10, 9);
+        instance.put(3, 26);
+        assertEquals(17, instance.get(8));
+        assertEquals(-1, instance.get(7));
+        assertEquals(18, instance.get(5));
+        instance.put(13, 17);
+        instance.put(2, 27);
+        instance.put(11, 15);
+        assertEquals(-1, instance.get(12));
+        instance.put(9, 19);
+        instance.put(2, 15);
+        instance.put(13, 16);
+        assertEquals(20, instance.get(1));
+        instance.put(12, 17);
+        instance.put(9, 1);
+        instance.put(6, 19);
+        assertEquals(29, instance.get(4));
+        assertEquals(18, instance.get(5));
+        assertEquals(18, instance.get(5));
+        instance.put(8, 1);
+        instance.put(11, 7);
+        instance.put(5, 2);
+        instance.put(9, 28);
+        assertEquals(20, instance.get(1));
+        instance.put(2, 2);
+        instance.put(7, 4);
+        instance.put(4, 22);
+        instance.put(7, 24);
+        instance.put(9, 26);
+        instance.put(13, 18);
+        instance.put(11, 26);
+    }
+
+    @Test
+    public void test10() {
+        A460LFUCache instance = new A460LFUCache(6);
+
+        instance.put(1, 1);
+        instance.put(2, 2);
+        instance.put(3, 3);
+        instance.put(4, 4);
+        instance.put(5, 5);
+        instance.put(6, 6);
+
+        assertEquals(1, instance.get(1));
+        assertEquals(2, instance.get(2));
+        assertEquals(3, instance.get(3));
+        assertEquals(4, instance.get(4));
+        assertEquals(3, instance.get(3));
+        assertEquals(4, instance.get(4));
+
+        int asd = 0;
+        int asdf = asd;
+    }
+
+    @Test
+    public void test11() {
+        A460LFUCache instance = new A460LFUCache(6);
+
+        instance.put(3, 3);
+        instance.put(2, 2);
+        instance.put(1, 1);
+        assertEquals(3, instance.get(3));
+        assertEquals(3, instance.get(3));
+        assertEquals(1, instance.get(1));
+
+        int asd = 0;
+        int asdf = asd;
+    }
 }
