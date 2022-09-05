600a19b33fc3a1856a4c0645a31f7d5a02ffdbd2
Nikolay Bespalov <nikolaybespalov@gmail.com>
2022-09-05 15:51:57 +0300

    102. Binary Tree Level Order Traversal

--- src/main/java/com/github/nikolaybespalov/leetcode/A102BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/github/nikolaybespalov/leetcode/A102BinaryTreeLevelOrderTraversal.java
@@ -3,7 +3,9 @@ package com.github.nikolaybespalov.leetcode;
 import com_github_leetcode.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @see <a href="https://leetcode.com/problems/binary-tree-level-order-traversal/">102. Binary Tree Level Order Traversal</a>
@@ -16,29 +18,27 @@ public class A102BinaryTreeLevelOrderTraversal {
             return ans;
         }
 
-        ArrayList<TreeNode> a = new ArrayList<>();
-        a.add(root);
-        while (!a.isEmpty()) {
-            ArrayList<TreeNode> b = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
-            for (TreeNode node : a) {
-                if (node.left != null) {
-                    b.add(node.left);
-                }
-                if (node.right != null) {
-                    b.add(node.right);
-                }
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> valuesAtLevel = new ArrayList<>();
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode n = queue.poll();
+                assert n != null;
+                valuesAtLevel.add(n.val);
+
+                if (n.left != null)
+                    queue.add(n.left);
+
+                if (n.right != null)
+                    queue.add(n.right);
             }
 
-            ArrayList<Integer> asd2 = new ArrayList<>(a.size());
-
-            for (TreeNode node : a) {
-                asd2.add(node.val);
-            }
-
-            ans.add(asd2);
-
-            a = b;
+            ans.add(valuesAtLevel);
         }
 
         return ans;
