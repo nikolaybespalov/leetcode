3044a92d51580cf8bff05f10f36b4b7591212111
Nikolay Bespalov <nikolaybespalov@gmail.com>
2022-09-08 09:18:41 +0300

    20. Valid Parentheses

--- src/main/java/com/github/nikolaybespalov/leetcode/A20ValidParentheses.java
+++ src/main/java/com/github/nikolaybespalov/leetcode/A20ValidParentheses.java
@@ -12,24 +12,18 @@ public class A20ValidParentheses {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '[' || c == '{')
                 stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                if (c == ')' && stack.peek() != '(')
-                    return false;
-
-                if (c == ']' && stack.peek() != '[')
-                    return false;
-
-                if (c == '}' && stack.peek() != '{')
-                    return false;
-
+            else if (stack.isEmpty())
+                return false;
+            else if (c == ')' && stack.peek() != '(')
+                return false;
+            else if (c == ']' && stack.peek() != '[')
+                return false;
+            else if (c == '}' && stack.peek() != '{')
+                return false;
+            else
                 stack.pop();
-            }
         }
 
         return stack.isEmpty();
